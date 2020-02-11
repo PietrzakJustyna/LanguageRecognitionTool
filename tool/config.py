86e2430911d6973a34700f9122b8f9869008fccf
Justyna Pietrzak <tynk713@gmail.com>
2020-02-11 21:51:16 +0100

    tool configuation

---
+++ tool/config.py
@@ -0,0 +1,27 @@
+max_letters = 20
+language_tags = {
+
+                'en': ['actor', 'alcohol', 'cheque', 'cancer', 'chocolate', 'debate', 'hobby', 'melon', 'propaganda',
+                      'religion', 'violin', 'england', 'MediaWiki'],
+
+                'pl': ['Warszawa', 'Europa', 'Pireneje', 'woda', 'koń', 'chrześcijanin', 'przyroda', 'ekosytem',
+                    'Zeus', 'Jan Pawel II', 'powstanie', 'filozofia', 'Zeus', 'kret', 'średniowiecze', 'łacina',
+                    'Fryderyk Chopin', 'Ogniem i Mieczem'],
+
+                'de': ['Deutsche_Sprache', 'Deutschland', 'Kommunistische_Partei_der_Sowjetunion', 'Wasser',
+                    'Festkörper', 'Seele', 'Geist', 'Dreifaltigkeit', 'Große', 'Christentum', 'Gott'],
+
+                'sv': ['Svenska', 'Sverige', 'Danmark', 'Europeiska_unionen', 'Medeltiden', 'Feodalism', 'Kung',
+                    'Kejsare', 'Monarki', 'Valmonarki', 'Choklad', 'Mjölk', 'Prolaktin', 'Kvinna', 'Eldvapen',
+                    'Kina', 'Götar', 'Romantiken', 'Ideologi', 'Tänkande', 'Pedagogik', 'Sekund', 'Solen', 'Väder',
+                    'Mellanöstern', 'Väte', 'Anatomi', 'Hjärta', 'Puls', 'Grekiska', 'Cypern'],
+
+                # 'fr': ['Français', 'Langues_romanes', 'Charlemagne', 'Traité_de_Verdun', 'Louis_le_Pieux',
+                #     'Son_(physique)', 'Zoologie', 'Intelligence_animale', 'Intelligence', 'Tautologie',
+                #     'Pléonasme', 'Figure_de_style']
+
+                'it': ['Lingua_italiana', 'Graffiti_(archeologia)', 'Impero_romano', 'Romolo_Augusto', 'Diritto_romano',
+                    'Europa', 'Continente', 'Islanda', 'Cioccolato', 'Alimento', 'Plantae', 'Aroma', 'Olfatto',
+                    'Organi_di_senso', 'Organismo_vivente', 'Epigenetica', 'Fenotipo', 'Composto_chimico',
+                    'Legame_covalente', 'Atomo', 'Materia_(fisica)', 'Energia', 'Fisica']
+                 }
